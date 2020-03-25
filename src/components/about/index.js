83fad02f2ab7057df91b7d82230cb252db028ba3
Jame <coynej1@wit.edu>
2020-03-24 22:04:57 -0400

    fixed navbar styling and about section style

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -7,11 +7,11 @@ function About(props) {
     return(
         <div>
             <Heading text="About"/>
-            <div class="containerAbout">
-                    <div class="rightAbout">
-                    <img src= {process.env.PUBLIC_URL + "me.jpg"} id="portrait"/>
+            <div class="row">
+                    <div class="colLeft">
+                        <img src= {process.env.PUBLIC_URL + "me.jpg"} id="portrait"/>
                     </div>
-                <div class="leftAbout">
+                <div class="colRight">
                     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc euismod purus orci, ac tristique nisl vestibulum facilisis. Sed in ex facilisis, malesuada augue at, bibendum lacus. In imperdiet libero non maximus ornare. Etiam est libero, vestibulum sed ultrices eu, placerat sed risus. Mauris maximus dolor at neque euismod semper. Cras eget ligula eget dui mattis vehicula ut vitae risus.</p>
                 </div>
             </div>
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -4,61 +4,28 @@ import { Link, animateScroll as scroll } from "react-scroll";
 import './index.scss';
 import '../../fonts/romans.ttf'
 
-function NavBar(props) {
-
+function NavItem(props) {
     return(
-            <div class="navbar">
-                
-                <div class="navHeading"> 
-                    <Link
-                    activeClass="active"
-                    to="splash"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                    class="navHeading"
-                    >
-                        JAME COYNE
-                    </Link>
-                </div>
-                {/* </Link> */}
-                <Link
-                    activeClass="active"
-                    to="about"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                    class="navHeading"
-                >
-                    <div class="navHeading">ABOUT</div>
-                </Link>
-                <Link
-                activeClass="active"
-                to="work"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                class="navHeading"
-                >
-                    <div class="navHeading">WORK</div>
-                </Link>
-                <Link
-                activeClass="active"
-                to="contact"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                class="navHeading"
-                >
-                <div class="navHeading">CONTACT</div>
-                </Link>
-            </div>
-    );
+        <Link
+            to={props.to}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            class="navItem">
+            {props.name}
+        </Link>
+    )
+}
 
+function NavBar(props) {
+    return(
+        <div class="navbar">
+            <NavItem name="JAME COYNE" to="splash"/>
+            <NavItem name="ABOUT" to="about"/>
+            <NavItem name="WORK" to="work"/>
+            <NavItem name="CONTACT" to="contact"/>
+        </div>
+    );
 }
 
 export default NavBar
